b1383bd78efe3818c62750e6ebf659474398aae3
huangyuan <yuanhuan@usc.edu>
2022-12-12 14:13:53 -0800

    script

--- src/OpenNMT-py/onmt/models/model.py
+++ src/OpenNMT-py/onmt/models/model.py
@@ -145,8 +145,9 @@ class LTEncModel(LongT5Model): #longT5 enc multichoice model
         #bottled_output = output.view(-1, output.size(2))
         scores = self.generator(output) #(batch_size,1)
         '''
-        #bottled_output = output.view(-1, output.size(2))
+
         bottled_output = self.classifier(output)
+        bottled_output = output.view(-1, output.size(2))
         scores = self.softmax(bottled_output)
 
 
