3183d1fb433e86a2432ce65cb882f351ad9baa7a
huangyuan <yuanhuan@usc.edu>
2022-12-13 03:42:06 -0800

    train

--- scripts/drop_qdmr_single_longt5.sh
+++ scripts/drop_qdmr_single_longt5.sh
@@ -5,7 +5,7 @@ export OPENNMT_PATH=../src/OpenNMT-py/                         # OpenNMT path
 export SPLIT=train                    # Number of exposure examples (1 or 100)
 export VSPLIT=validation
 export SAVE_PATH=${OPENNMT_PATH}/tf_checkpoints # Save path for checkpoints
-export SAVE_NAME=drop_qdmr_single_longt5      # Checkpoint name
+export SAVE_NAME=quality_longt5      # Checkpoint name
 export LOG_PATH=${OPENNMT_PATH}/logs           # Log path
 export PRED_PATH=${OPENNMT_PATH}/preds         # Predictions path
 export SEED=1                                  # Random seed
@@ -22,7 +22,7 @@ do
         -valid_data $DATA_PATH/${VSPLIT}_source.txt -valid_datat $DATA_PATH/${VSPLIT}_target.txt \
         -train_steps 30000 -valid_steps 500 -save_checkpoint_steps 500 -early_stopping 3 \
         -batch_size 2 -accum_count 32 -learning_rate 1e-3 \
-        -task_name 'question' -architect longt5 -load_t5 -load_lm -trim_size 4096 \
+        -task_name 'question' -architect longt5 -load_t5 -trim_size 4096 \
         -save_model $SAVE_PATH/${SAVE_NAME}/s$SEED -layers 2 -rnn_size 512 -word_vec_size 512 -transformer_ff 512 -heads 4  \
         -encoder_type transformer -decoder_type transformer -position_encoding \
         -max_generator_batches 2 -dropout 0.1 -batch_type sents -normalization sents \
@@ -32,7 +32,7 @@ do
         -world_size 1 -gpu_ranks 0 -seed $SEED --log_file ${LOG_PATH}/${SAVE_NAME}_s${SEED}.log
 
     # evaluation
-    for SPLIT in qdmr_single_dev
+    for SPLIT in validation
     do
         python $OPENNMT_PATH/translate.py -model $SAVE_PATH/${SAVE_NAME}/s${SEED}_best.pt \
                                           -src $DATA_PATH/${SPLIT}_source.txt \
