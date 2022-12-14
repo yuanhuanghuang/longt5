f93ab0906a9c585c80f12d7fee954de6c8c0d7aa
huangyuan <yuanhuan@usc.edu>
2022-12-14 00:34:05 -0800

    task_name

--- scripts/drop_qdmr_single_longt5.sh
+++ scripts/drop_qdmr_single_longt5.sh
@@ -5,7 +5,7 @@ export OPENNMT_PATH=../src/OpenNMT-py/                         # OpenNMT path
 export SPLIT=train                    # Number of exposure examples (1 or 100)
 export VSPLIT=validation
 export SAVE_PATH=${OPENNMT_PATH}/tf_checkpoints # Save path for checkpoints
-export SAVE_NAME=quality_longt5      # Checkpoint name
+export SAVE_NAME=quality_longt5_8192      # Checkpoint name
 export LOG_PATH=${OPENNMT_PATH}/logs           # Log path
 export PRED_PATH=${OPENNMT_PATH}/preds         # Predictions path
 export SEED=1                                  # Random seed
@@ -20,9 +20,9 @@ do
     # training
     python -m pdb $OPENNMT_PATH/train.py -mode mc -train_data $DATA_PATH/${SPLIT}_source.txt -train_datat $DATA_PATH/${SPLIT}_target.txt \
         -valid_data $DATA_PATH/${VSPLIT}_source.txt -valid_datat $DATA_PATH/${VSPLIT}_target.txt \
-        -train_steps 30000 -valid_steps 500 -save_checkpoint_steps 500 -early_stopping 3 \
+        -train_steps 30000 -valid_steps 100 -save_checkpoint_steps 500 -early_stopping 10 \
         -batch_size 2 -accum_count 32 -learning_rate 1e-3 \
-        -task_name 'question' -architect longt5 -load_t5 -trim_size 4096 \
+        -task_name 'mc' -architect longt5 -load_t5 -trim_size 8192 \
         -save_model $SAVE_PATH/${SAVE_NAME}/s$SEED -layers 2 -rnn_size 512 -word_vec_size 512 -transformer_ff 512 -heads 4  \
         -encoder_type transformer -decoder_type transformer -position_encoding \
         -max_generator_batches 2 -dropout 0.1 -batch_type sents -normalization sents \
